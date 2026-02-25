d02777ca7269a8a03d0c2f046e1e9d9bf97f865c
Divyansh Arora <divyansh82188@gmail.com>
2026-02-25 22:16:32 +0530

    first part

---
+++ draw-app/apps/http-backend/src/middleware.ts
@@ -0,0 +1,22 @@
+import { Request,Response,NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+
+export function middleware(req:Request,res:Response,next:NextFunction){
+
+const token=req.headers["authorization"]??" ";
+
+const decoded=jwt.verify(token,JWT_SECRET) as{userId:string}
+
+if(decoded){
+    req.userId=decoded.userId
+    next();
+
+}
+else{
+    res.status(403).json({
+        message:"not authorized",
+    })
+}
+}
